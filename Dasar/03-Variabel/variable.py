34be0ddc34141029f3fc1570bcd01cbd25a5b0fa
Fajryan Syahputra <133894141+fajryansyahputra@users.noreply.github.com>
2023-05-20 14:10:13 +0700

    Sumber Kode Variabel Pada Python

--- Dasar/03-Variabel/variable.py
+++ Dasar/03-Variabel/variable.py
@@ -7,7 +7,8 @@ nilai = 95  # Nilai
 print("Dia bernama ", nama) 
 print("Dia berusia ", umur, " tahun")
 print("Dia mendapatkan nilai ", nilai)
-print("Oleh karena itu, Dinyatakan LULUS")
+print("Oleh karena itu, dinyatakan LULUS")
+print() # Untuk menambah baris baru (tidak harus)
 
 # Contoh penggunaan variabel yang dilarang.
 # 1angka = 1  karena dimulai dari angka.
@@ -17,14 +18,17 @@ print("Oleh karena itu, Dinyatakan LULUS")
 # nama_lengkap2 = "Andi Hartuni"
 # nama_lengkap_2 = "Andi Hartono"
 
+
 # Contoh inisiasi beberapa variable dalam satu line.
 a, b, c = 1, 2, 3
 print("a adalah nomor ", a)  # 1
 print("b adalah nomor ", b)  # 2
 print("c adalah nomor ", c)  # 3
+print() # Untuk menambah baris baru (tidak harus)
 
 # Contoh penggunaan satu value dalam beberapa variabel.
 d = e = f = 1
 print("d adalah nomor ", d)  # 1
 print("e adalah nomor ", e)  # 1
 print("f adalah nomor ", f)  # 1
+print() # Untuk menambah baris baru (tidak harus)
