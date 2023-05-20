c064a9e3c9b954c3020feac8fedffd7914552e56
Fajryan Syahputra <133894141+fajryansyahputra@users.noreply.github.com>
2023-05-20 14:06:09 +0700

    Sumber Kode Variabel Pada Python

---
+++ Dasar/03-Variabel/variable.py
@@ -0,0 +1,30 @@
+# Belajar Variable
+nama = "Andi"  # Contoh tipe data string (str)
+umur = 17  # Contoh tipe data integer (int)
+nilai = 95  # Nilai
+
+# Tanda koma ',' pada print digunakan untuk menambahkan nilai variabel ke perintah print.
+print("Dia bernama ", nama) 
+print("Dia berusia ", umur, " tahun")
+print("Dia mendapatkan nilai ", nilai)
+print("Oleh karena itu, Dinyatakan LULUS")
+
+# Contoh penggunaan variabel yang dilarang.
+# 1angka = 1  karena dimulai dari angka.
+
+# Contoh penggunaan variable yang baik.
+# nama_lengkap = "Andi Hartani"
+# nama_lengkap2 = "Andi Hartuni"
+# nama_lengkap_2 = "Andi Hartono"
+
+# Contoh inisiasi beberapa variable dalam satu line.
+a, b, c = 1, 2, 3
+print("a adalah nomor ", a)  # 1
+print("b adalah nomor ", b)  # 2
+print("c adalah nomor ", c)  # 3
+
+# Contoh penggunaan satu value dalam beberapa variabel.
+d = e = f = 1
+print("d adalah nomor ", d)  # 1
+print("e adalah nomor ", e)  # 1
+print("f adalah nomor ", f)  # 1
